a498d96117f7cf5b90ea39569c397b60586d168e
MrChike <chikeegonu@gmail.com>
2020-02-11 03:07:33 +0100

    User Registration & Authentication view created

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,14 @@
+from projects import views
+from django.urls import path
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from projects import views
 
 
 router = DefaultRouter()
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('users/register', views.CreateUserView.as_view()),
+    path('users/auth', views.AuthenticateUserView.as_view()),
 ]
